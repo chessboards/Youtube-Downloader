f6e6e8cae147ca98ccf9d08890460c8f75e0a810
chessboards <64972947+chessboards@users.noreply.github.com>
2021-10-01 15:13:55 -0400

    Update main.py
    implemented audio downloading, fixed illegal characters on windows

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-# Notes:
-    # notetoself: https://code.visualstudio.com/docs/python/environments
-    #             https://pytube.io/en/latest/user/streams.html#downloading-streams
-    
 from pytube import YouTube
+import re
 
+# note to self: https://code.visualstudio.com/docs/python/environments
+#               use debugger to avoid execution policy shenanigans
+
+videoChoices = ["v", "video", "0", "vid", "mp4"]
+audioChoices = ["a", "audio", "1", "aud", "sound", "mp3"]
 
 index = 0
 
 def progressCallback(_stream, _chunk, bytes_remaining):
+    """Display to the user the bytes remaining along with an animation while they wait"""
     global index
     animationReel = ["/", ".", "\\"]
     
@@ -18,26 +21,55 @@ def progressCallback(_stream, _chunk, bytes_remaining):
     print(f"{animationReel[index]} | {bytes_remaining} bytes remaining...")
     
     index += 1
+   
+def sanitizeTitle(title):
+    """Sanitizes the passed title string to allow the file to be saved on windows without introducing illegal characters"""
+    matches = re.findall(r"[^/\\:*?\"<>|]+", title) 
+
+    fullstring = ""
+    for match in matches:
+        fullstring += match
+        
+    return fullstring
     
     
-if __name__ == "__main__":
-    # vars
+def main():
+    """Main function of the program called by the entry point."""
+    ## User input entries ##
+    
     link = input("Enter youtube url: ")
-    destin = input("Full path destination: ")
+    filetype = input("Video or audio?: ")
+    
+    extension = ""
+    if filetype.lower() in videoChoices: extension = "mp4"
+    if filetype.lower() in audioChoices: extension = "webm" # lib only allows webm
+    else:
+        print("Error: Invalid filetype.")
+        return # exit program
+        
     filename = input("File name (leave blank to use video title): ")
+    destin = input("Full path destination: ")
 
     video = YouTube(link, progressCallback)
-    stream = (
-                video.streams
-                             .filter(file_extension='mp4')
-                             .get_highest_resolution()
-             )
-
+    stream = "" # empty for scope
+    
+    if extension == "webm":
+        stream = video.streams.filter(only_audio = True, subtype="webm").first() # best with supplied params
+    else:
+        stream = video.streams.filter(file_extension = extension).get_highest_resolution() # best video resolution
+        
     # default filename to title
     if (filename == "" or filename == " "):
-        filename = video.title
+        filename = sanitizeTitle(video.title) # sanitize the youtube title for windows
         
+    ## Begin downloading... ##
+    
     print(f"\nDownloading {video.title} by {video.author}")
 
-    stream.download(destin, filename + ".mp4") # extension not automatically added
+    stream.download(destin, f"{filename}.{extension}") # extension not automatically added
     print("Done!")
+
+
+# entry point
+if __name__ == "__main__":
+    main()
